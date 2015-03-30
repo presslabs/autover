8b44af3b7a310201b6f4bf774c9709b4342536ed
Krody Robert <robi@presslabs.com>
2015-03-30 12:26:28 +0300

    Changed version

--- autover.php
+++ autover.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/autover/
  * Description: Automatically version your CSS and JS files.
  * Author: PressLabs
- * Version: 1.4
+ * Version: 1.4.1
  * Author URI: http://www.presslabs.com/
  */
 
